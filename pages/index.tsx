937203a9748871c32c5ccb4a2e47f72490ba9cb6
skdev <saeed.karimi@generalinternet.ca>
2022-10-22 12:28:30 -0700

    post grid done

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,11 +31,19 @@ export default function Home({ posts }: Props) {
 	  </div>
 
 	{/* if posts not undefined */}
-	<div>
+	<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
 		{posts && posts.map((post) => (
 			<Link href={`/blog/${post.slug.current}`} key={post._id}>
-				<div>
-					<img src={urlFor(post.mainImage).url()} alt={post.title} />
+				<div className="border rounded-lg group cursor-pointer overflow-hidden">
+					<img className="h-60 w-full object-cover group-hover:scale-105 transition duration-200" 
+					src={urlFor(post.mainImage).url()} alt={post.title} />
+					<div className="flex justify-between p-5 bg-white">
+						<div>
+							<p className="text-lg font-bold">{post.title}</p>
+							<p className="text-xs">{post.description} by {post.author.name}</p>
+						</div>
+						<img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()!} alt={post.author.name} />
+					</div>
 				</div>
 			</Link>
 		))}
