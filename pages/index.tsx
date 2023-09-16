8a822ae365cf85c9c86edad65032d3c9f9124ab9
Saeed Karimi <saeedsektor@gmail.com>
2023-09-16 08:51:53 -0700

    added order to posts query

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home({ posts }: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>skdev | home</title>
+        <title>SKDEV | Blog</title>
 		<link rel="icon" href="/favicon.png" />
       </Head>
 	  <Header />
@@ -54,18 +54,18 @@ export default function Home({ posts }: Props) {
 }
 
 export const getServerSideProps = async () => {
-	const query = `*[_type == "post"]{
-	  _id,
-	  title,
-	  slug,
-	  author -> {
-		name,
-		image
-	  },
-	  description,
-	  mainImage,
-	  publishedAt
-	}`;
+    const query = `*[_type == "post"] | order(publishedAt desc){
+        _id,
+        title,
+        slug,
+        author -> {
+            name,
+            image
+            },
+        description,
+        mainImage,
+        publishedAt
+    }`;
 
 	const posts = await sanityClient.fetch(query);
 
