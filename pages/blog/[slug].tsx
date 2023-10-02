a4f2f4a85d59c14c81e218d402f374777dcac58f
Saeed Karimi <saeedsektor@gmail.com>
2023-10-02 10:36:34 -0700

    fixed the types error for syntax highlighter

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,7 +7,6 @@ import PortableText from "react-portable-text";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useState } from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface IFormInput {
 	_id: string;
